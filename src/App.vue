a4af0b70e1020bf60dfb8cb4d8fca46d024ce86f
rivascristian28@gmail.com <rivascristian28@gmail.com>
2023-10-03 19:01:14 -0500

    Number one commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a href=""> <img src="../public/img2/logo.png" alt=""></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../src/components/CamisasBelligham.vue">Camisas Belligham</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../src/components/CamisasVini">Camisas Vinicius jr</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../src/components/CamisasModric.vue">Camisas Luka Modrid</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, DefineComponent } from 'vue';
+
+import CamisasBelligham from './components/CamisasBelligham.vue'
+
+import CamisasModric from './components/CamisasModric.vue'
+
+import CamisasVini from './components/CamisasVini.vue'
+
+export default defineComponent({
+  components: {
+    CamisasBelligham
+    CamisasModric
+    CamisasVini
+  }
+})
+
+
+
+</script>
+
+<style lang="ts" scoped>
+
+
+
+</style>
---
+++ src/components/CamisasModric.vue
@@ -0,0 +1,49 @@
+<template>
+    <h2> camisas  Lucka Modrid</h2>
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+<div class="carousel-indicators">
+  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+</div>
+<div class="carousel-inner">
+  <div class="carousel-item active">
+    <img src="../public/img2/8(1).png" class="d-block w-100" alt="..." width="600px" height="600px">
+    <div class="carousel-caption d-none d-md-block">
+      <h5>Camisa de Toteham</h5>
+      <p>Hermosa camisa exclusiva solo aqui en Hala madrid</p>
+    </div>
+  </div>
+  <div class="carousel-item">
+    <img src="../public/img2/10.png" class="d-block w-100" alt="..." width="600px" height="600px">
+    <div class="carousel-caption d-none d-md-block">
+      <h5>Camisa Real madrid</h5>
+      <p>Hermosa camisa exclusiva solo aqui en Hala madrid</p>
+    </div>
+  </div>
+  <div class="carousel-item">
+    <img src="../public/img2/9.png" class="d-block w-100" alt="..." width="600px" height="600px">
+    <div class="carousel-caption d-none d-md-block">
+      <h5>Camisa de Croacia</h5>
+      <p>Hermosa camisa exclusiva solo aqui en Hala madrid</p>
+    </div>
+  </div>
+</div>
+<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+  <span class="visually-hidden">Previous</span>
+</button>
+<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  <span class="visually-hidden">Next</span>
+</button>
+</div>
+</template>
+
+<script lang="ts" scoped>
+
+</script>
+
+<style lang="ts" scoped>
+
+</style>
